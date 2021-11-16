214cd71a7174c06f32a50fdafb38500ccb964c67
Aidan717 <66300475+Aidan717@users.noreply.github.com>
2021-11-16 16:24:55 +0100

    solitaire shuffle
    card shuffle as solitaire should + a coroutine for deal effect.

--- EasySolitaire/Assets/Scripts/Solitaire.cs
+++ EasySolitaire/Assets/Scripts/Solitaire.cs
@@ -1,19 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Solitaire : MonoBehaviour
 {
     public Sprite[] cardFaces;
     public GameObject cardPrefab;
+    public GameObject[] bottomPos;
+    public GameObject[] topPos;
+
 
     public static string[] suits = new string[] { "C", "D", "H", "S" };
     public static string[] values = new string[] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+    public List<string>[] bottoms;
+    public List<string>[] tops;
+    
+    private List<string> bottom0 = new List<string>();
+    private List<string> bottom1 = new List<string>();
+    private List<string> bottom2 = new List<string>();
+    private List<string> bottom3 = new List<string>();
+    private List<string> bottom4 = new List<string>();
+    private List<string> bottom5 = new List<string>();
+    private List<string> bottom6 = new List<string>();
+
 
     public List<string> deck;
     // Start is called before the first frame update
     void Start()
     {
+        bottoms = new List<string>[] {bottom0, bottom1, bottom2, bottom3, bottom4, bottom5, bottom6};
         PlayCards();
     }
 
@@ -31,7 +47,8 @@ public class Solitaire : MonoBehaviour
         foreach (string card in deck) {
             print(card);
         }
-        SolitaireDeal();
+        SolitaireSort();
+        StartCoroutine(SolitaireDeal());
     }
 
     public static List<string> GenerateDeck() {
@@ -56,15 +73,33 @@ public class Solitaire : MonoBehaviour
         }
     }
 
-    void SolitaireDeal() {
-        float yOffset = 0;
-        float zOffset = 0.03f;
-        foreach (string card in deck) {
-            GameObject newCard = Instantiate(cardPrefab, new Vector3(transform.position.x, transform.position.y - yOffset, transform.position.z - zOffset), Quaternion.identity);
-            newCard.name = card;
+    IEnumerator SolitaireDeal() {
 
-            yOffset = yOffset + 0.1f;
-            zOffset = zOffset + 0.03f;
+        for (int i = 0; i < 7; i++) {
+
+            float yOffset = 0;
+            float zOffset = 0.03f;
+            foreach (string card in bottoms[i]) {
+                yield return new WaitForSeconds(0.01f);
+                GameObject newCard = Instantiate(cardPrefab, new Vector3(bottomPos[i].transform.position.x, bottomPos[i].transform.position.y - yOffset, bottomPos[i].transform.position.z - zOffset), Quaternion.identity, bottomPos[i].transform);
+                newCard.name = card;
+                if (card == bottoms[i][bottoms[i].Count - 1]) {
+                    newCard.GetComponent<Selectable>().faceUp = true;
+                }
+                 
+
+                yOffset = yOffset + 0.15f;
+                zOffset = zOffset + 0.03f;
+            }
+        }
+    }
+
+    void SolitaireSort() {
+        for (int i = 0; i < 7; i++) {
+            for (int j = i; j < 7; j++) {
+                bottoms[j].Add(deck.Last<string>());
+                deck.RemoveAt(deck.Count - 1);
+            }
         }
     }
 }
